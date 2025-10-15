37353fea9882a088d014ad5c7886a533194041e4
082588256 <daipthe160387@fpt.edu.vn>
2025-10-15 12:00:34 +0700

    organize folder structure

---
+++ Capstone_Project/Domain/Models/TourImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE_Capstone_Project.Domain.Models;
+
+public partial class TourImage
+{
+    public int Id { get; set; }
+
+    public int TourId { get; set; }
+
+    public string? Image { get; set; }
+
+    public virtual Tour Tour { get; set; } = null!;
+}
---
+++ Capstone_Project/Domain/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE_Capstone_Project.Domain.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? PasswordHash { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Image { get; set; }
+
+    public int RoleId { get; set; }
+
+    public byte? UserStatus { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual ICollection<Chat> ChatCustomers { get; set; } = new List<Chat>();
+
+    public virtual ICollection<Chat> ChatStaffs { get; set; } = new List<Chat>();
+
+    public virtual ICollection<News> News { get; set; } = new List<News>();
+
+    public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual ICollection<Wishlist> Wishlists { get; set; } = new List<Wishlist>();
+}
