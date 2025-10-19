66ce3558cfec840fa80d1e27035b40f6221b3230
duylp123 <hehi5527@gmail.com>
2025-10-19 12:54:33 +0700

    push

---
+++ FE_Capstone_Project/Controllers/StaffController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BE_Capstone_Project.Controllers
+{
+    //[Authorize] // Nếu cần authentication
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Staff Dashboard";
+            return View();
+        }
+
+        public IActionResult Tours()
+        {
+            ViewData["Title"] = "Quản lý Tour";
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+
+        public IActionResult Bookings()
+        {
+            ViewData["Title"] = "Quản lý Đặt tour";
+            return View();
+        }
+
+        public IActionResult Customers()
+        {
+            ViewData["Title"] = "Quản lý Khách hàng";
+            return View();
+        }
+
+        public IActionResult Schedules()
+        {
+            ViewData["Title"] = "Lịch trình Tour";
+            return View();
+        }
+
+        public IActionResult Tasks()
+        {
+            ViewData["Title"] = "Công việc của tôi";
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            ViewData["Title"] = "Hồ sơ cá nhân";
+            return View();
+        }
+    }
+}
