a40bd031deea78274c04e80e38dd5a65a743c10d
Malevian <achyn2004@gmail.com>
2025-11-03 00:12:37 +0700

    Minor fix

--- Capstone_Project/Domain/Enums/ScheduleStatus.cs
+++ Capstone_Project/Domain/Enums/ScheduleStatus.cs
@@ -5,9 +5,9 @@ namespace BE_Capstone_Project.Domain.Enums
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ScheduleStatus : byte
     {
-        Scheduled = 1,
-        InProgress = 2,
-        Completed = 3,
-        Cancelled = 4
+        Cancelled = 0,
+        Completed = 1,
+        Ongoing = 2,
+        Scheduled = 3,
     }
 }
